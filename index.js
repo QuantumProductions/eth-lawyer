d839b93fa0209d14152562af7918c8c7fbce9d2b
quantumpotato <quantumpotato@gmail.com>
2018-05-17 16:08:17 -0400

    0.0.1 development

---
+++ index.js
@@ -0,0 +1,43 @@
+const PubSub = require('pubsub-js');
+
+class EthLawyer {
+  constructor(address, abi) {
+    let web3 = window.web3;
+    
+    let MyContract = web3.eth.contract(abi);
+    this.contract = MyContract.at(address);
+
+    this.lastAddress = null;
+    this.loadAccounts();
+  }
+
+  loadAccounts() {
+    let web3 = window.web3;
+    if (web3) {
+      web3.eth.getAccounts(function(a,b) {
+        if (b && b.length > 0) {
+          this.accountLoaded(b[0]);
+        } else {
+          this.accountLoaded(null);
+          return;
+        }
+      }.bind(this));  
+    } else {
+      Pubsub.publish('eth-lawyer-account', {address: null, lastAddress: this.lastAddress, hasMetamask: false});
+      this.lastAddress = null;
+    }
+
+    setTimeout(this.loadAccounts.bind(this), 5000);
+  }
+
+  accountLoaded(address) {
+    if (!address) {
+      Pubsub.publish('eth-lawyer-account', {address: null, lastAddress: this.lastAddress, hasMetamask: true});
+    } else {
+      PubSub.publish('eth-lawyer-account', {address: address, lastAddress: this.lastAddress, hasMetamask: true);
+    }
+    this.lastAddress = address;
+  }
+}
+
+module.exports = EthLawyer;
